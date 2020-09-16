55f5fab2b828d11fa77718daa314e12eb136d203
Jeanette <jbrasepae@gmail.com>
2020-09-16 16:20:14 +0200

    fixed sypOn methods

--- spec/node_sql_spec.js
+++ spec/node_sql_spec.js
@@ -1,37 +1,43 @@
-const {pool,createTable,addNewVisitor,deleteVisitor, updateVisitor, viewOneVisitor, deleteAllVisitors, listAllVisitors} = require('../src/node_sql')
+const {addNewVisitor,createTable,listAllVisitors, deleteVisitor, updateVisitor, viewOneVisitor, deleteAllVisitors} = require('../src/node_sql')
 
 describe("Visitors mock test", () => {
     it("should check if the createTable is defined", function() {
         expect(createTable).toBeDefined();
       });
     it("spyOn addNewVisitor function", () => {
-      spyOn(pool, 'query');
-      addNewVisitor();
-      expect(pool.query).toHaveBeenCalled();
+      var newVisitor = {addNewVisitor};
+      spyOn(newVisitor, 'addNewVisitor');
+      newVisitor.addNewVisitor('Tumi',21,'2020-10-15','11:59:05','Vicent Jacobs','great service');
+      expect(newVisitor.addNewVisitor).toHaveBeenCalled();
     });
     it("should have been called", () => {
-        spyOn(pool, "query");
-        listAllVisitors()
-        expect(pool.query).toHaveBeenCalled();
+        var list = {listAllVisitors};
+        spyOn(list, "listAllVisitors");
+        list.listAllVisitors();
+        expect(list.listAllVisitors).toHaveBeenCalled();
     });
     it("should delete a visitor", () => {
-        spyOn(pool, "query");
-        deleteVisitor()
-        expect(pool.query).toHaveBeenCalled();
+        var deletes = {deleteVisitor};
+        spyOn(deletes, "deleteVisitor");
+        deletes.deleteVisitor();
+        expect(deletes.deleteVisitor).toHaveBeenCalled();
     });
     it("should update visitor", () => {
-        spyOn(pool, "query");
-        updateVisitor()
-        expect(pool.query).toHaveBeenCalled();
+        var updates = {updateVisitor};
+        spyOn(updates, "updateVisitor");
+        updates.updateVisitor();
+        expect(updates.updateVisitor).toHaveBeenCalled();
     });
     it("should view one visitor", () => {
-        spyOn(pool, "query");
-        viewOneVisitor()
-        expect(pool.query).toHaveBeenCalled();
+        var viewVisitor = {viewOneVisitor};
+        spyOn(viewVisitor, "viewOneVisitor");
+        viewVisitor.viewOneVisitor();
+        expect(viewVisitor.viewOneVisitor).toHaveBeenCalled();
     });
     it("should delete all visitor", () => {
-        spyOn(pool, "query");
-        deleteAllVisitors()
-        expect(pool.query).toHaveBeenCalled();
+        var deleteAll = {deleteAllVisitors}
+        spyOn(deleteAll, "deleteAllVisitors");
+        deleteAll.deleteAllVisitors();
+        expect(deleteAll.deleteAllVisitors).toHaveBeenCalled();
     });
 });
